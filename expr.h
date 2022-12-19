0693c143db6252d96ff6a5da56a72ba8f59b1770
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-12-19 16:20:42 +0000

    More fixes

--- expr.h
+++ expr.h
@@ -4,7 +4,7 @@
 #include <optional>
 #include "idents.h"
 
-class Stmt {}; //Expressions return something
+class Expr {}; //Expressions return something
 class Stmt {}; //Statements don't
 
 
@@ -19,13 +19,14 @@ private:
 };
 
 class MultiStmt: public Stmt {
-    MultiStmt(std::vector<std::unique_ptr<Stmt>> exprs) : exprs(std::move(exprs)) {}
+public:
+    MultiStmt(std::vector<std::unique_ptr<Stmt>>& stmts) : stmts(std::move(stmts)) {}
 
 private:
-    std::vector<std::unique_ptr<Stmt>> exprs;
+    std::vector<std::unique_ptr<Stmt>> stmts;
 };
 
-class ConstExpr: public Stmt {
+class ConstExpr: public Expr {
 public:
     ConstExpr(std::string token): token(token) {}
 
@@ -35,28 +36,28 @@ private:
 
 class SetStmt: public Stmt {
 public:
-    SetStmt(Variable& lval, Stmt& rval) : lval(lval), rval(rval) {}
+    SetStmt(Variable& lval, Expr& rval) : lval(lval), rval(rval) {}
 
 private:
     Variable& lval;
-    Stmt& rval;
+    Expr& rval;
 };
 
 class ReturnStmt: public Stmt {
 public:
-    ReturnStmt(Stmt& rval) : rval(rval) {}
+    ReturnStmt(Expr& rval) : rval(rval) {}
 
 private:
-    Stmt& rval;
+    Expr& rval;
 };
 
 class FunctionDef: public Stmt {
 public:
     FunctionDef(Type return_type,
                  std::string name,
-                 std::vector<std::unique_ptr<Variable>> args,
+                 std::vector<std::unique_ptr<Variable>>& args,
                  std::optional<std::string> arena,
-                 std::unique_ptr<MultiStmt> body) :
+                 std::unique_ptr<MultiStmt>& body) :
                  return_type(return_type), name(name),
                  args(std::move(args)), arena(arena), body(std::move(body)) {}
 
